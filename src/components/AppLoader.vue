b20627583faf1ede5cd0866ff77d78f90e624f0e
carloeusebi <carloeusebi@gmail.com>
2023-09-11 15:51:32 +0200

    Created loader component

---
+++ src/components/AppLoader.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="layover">
+        <font-awesome-icon class="text-white" :icon="['fas', 'circle-notch']" spin
+            size="2xl" />
+    </div>
+</template>
+
+<style scoped>
+.layover {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    position: fixed;
+    inset: 0;
+
+    background-color: rgba(0, 0, 0, .75);
+}
+</style>
