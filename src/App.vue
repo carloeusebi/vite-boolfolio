f676aeb68e534d857537cf72e19bd3f0dd164018
carloeusebi <carloeusebi@gmail.com>
2023-09-11 15:04:14 +0200

    Project setup

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <script setup>
 </script>
 
-<template></template>
+<template>
+  <div class="container">
+
+    <h1 class="text-center my-5">Hello World</h1>
+  </div>
+</template>
 
 <style scoped></style>
