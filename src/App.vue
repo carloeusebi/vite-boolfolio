2cccbedfa2c91ffae4646963bb4ebf0239a1dd18
carloeusebi <carloeusebi@gmail.com>
2023-09-11 15:12:51 +0200

    Ajax call to backend

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,23 @@
 <script setup>
+import { onMounted, ref } from 'vue';
+import axiosInstance from '@/axios.js';
+import { isAxiosError } from 'axios';
+
+const projects = ref([]);
+
+onMounted(async () => {
+  // call the api to fetch the first projects
+  try {
+    //set the loader
+    const { data } = await axiosInstance.get('/projects');
+
+    console.log(data);
+  } catch (err) {
+    //todo errors
+  } finally {
+    //todo unset the loader
+  }
+})
 </script>
 
 <template>
