db2798d021154e9865587e3cf26a9a3d336940dc
carloeusebi <carloeusebi@gmail.com>
2023-09-12 15:57:45 +0200

    Written logic to fetch project

--- src/pages/ProjectDetails.vue
+++ src/pages/ProjectDetails.vue
@@ -1,9 +1,31 @@
-<script lang="ts" setup>
+<script setup>
+import { useRoute } from 'vue-router';
+import axiosInstance from '@/axios.js';
+import { onMounted, ref } from 'vue';
+
+const id = useRoute().params.id;
+const project = ref(null);
+const url = `http://localhost:8000/api/projects/${id}`
+
+const fetchProject = async () => {
+    // set loader
+    try {
+        const { data } = await axiosInstance.get(url);
+        project.value = data;
+        console.log(project.value)
+    } catch (err) {
+
+    } finally {
+        // unset loader
+    }
+}
+
+onMounted(() => {
+    fetchProject();
+})
 
 </script>
 
-<template>
-    Project details
-</template>
+<template></template>
 
 <style scoped></style>
