a39d93db20ad4c72680e93f0732b2982f6dfd0cd
carloeusebi <carloeusebi@gmail.com>
2023-09-12 16:16:02 +0200

    Implemented error handling on project details page

--- src/pages/ProjectDetails.vue
+++ src/pages/ProjectDetails.vue
@@ -3,19 +3,32 @@ import { useRoute } from 'vue-router';
 import axiosInstance from '@/axios.js';
 import { onMounted, ref } from 'vue';
 import { loader } from '../stores/loader';
+import { isAxiosError } from 'axios';
+import AppAlert from '../components/AppAlert.vue';
 
 const id = useRoute().params.id;
 const project = ref(null);
 const url = `http://localhost:8000/api/projects/${id}`
+const alertConfig = ref({
+    type: 'info',
+    message: `No project found with ID ${id}`
+})
 
 const fetchProject = async () => {
     loader.setLoader();
     try {
         const { data } = await axiosInstance.get(url);
         project.value = data;
-        console.log(project.value)
     } catch (err) {
-
+        // not very friendly for the customer, i know, but this shows error code and message, just to do something different :)
+        if (isAxiosError(err) && err.response.status !== 404) {
+            alertConfig.value.type = 'warning';
+            alertConfig.value.message = `Error: ${err.response.status}: ${err.response.data.message}`
+        }
+        else {
+            //if it is not axios error just console log the error for debugging.
+            console.error(err);
+        }
     } finally {
         loader.unsetLoader();
     }
@@ -30,7 +43,13 @@ onMounted(() => {
 <template>
     <div v-if="!loader.isLoading">
         <!-- temporary dump -->
-        {{ project }}
+        <div v-if="project">
+            {{ project }}
+        </div>
+        <div v-else>
+            <AppAlert :config="alertConfig" />
+        </div>
+
     </div>
 </template>
 
